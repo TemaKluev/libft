68099022b9db6d5774c7d514bd7664e2c8684bbb
Artem Kliuiev <akliuiev@c3r2s2.42wolfsburg.de>
2024-06-16 19:44:22 +0200

    from school 16.06 v2

---
+++ ft_strrchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akliuiev <akliuiev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
+/*   Updated: 2024/06/16 19:43:23 by akliuiev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strrchr(const char *s, int c)
+{
+    int	i;
+
+	i = ft_strlen(s);
+	while (i > 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i--;
+	}
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	else
+		return (NULL);
+}
