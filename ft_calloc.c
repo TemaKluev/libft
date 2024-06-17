8a557f6517088d562ba9fa1ed582a218d53b9edc
Artem Kliuiev <tartem_09@live.com>
2024-06-17 21:20:13 +0200

    from laptop 17.06

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artemkliuiev <artemkliuiev@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
+/*   Updated: 2024/06/17 19:15:53 by artemkliuie      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	while (size)
+	{
+		size--;
+		ptr[size] = '\0';
+	}
+	return (ptr);
+}
