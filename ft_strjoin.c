8005b16a4e98d6231db0442e5bdcaa3e3c3219d4
Artem Kliuiev <akliuiev@c2r2s5.42wolfsburg.de>
2024-06-18 22:03:05 +0200

    efa

---
+++ ft_strjoin.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akliuiev <akliuiev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
+/*   Updated: 2024/06/18 21:45:28 by akliuiev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = ft_strlen(s1);
+	i = i + ft_strlen(s2);
+	str = malloc(sizeof(char) * i + 1);
+	if (str = 0)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		str[j] = s1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str[j] = s2[i];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
