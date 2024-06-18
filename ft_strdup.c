90e473d05a5e09926eb3159b12dcf41d5e9d8240
Artem Kliuiev <tartem_09@live.com>
2024-06-18 20:39:58 +0200

    mfpwemf

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artemkliuiev <artemkliuiev@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
+/*   Updated: 2024/06/17 23:09:45 by artemkliuie      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(const char *s)
+{
+	int		i;
+	char	*str;
+
+	i = ft_strlen (s);
+	str = malloc(sizeof(char) * (i + 1));
+	if (str == 0)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ ft_substr.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artemkliuiev <artemkliuiev@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
+/*   Updated: 2024/06/17 23:18:40 by artemkliuie      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	int		i;
+	char	*str;
+
+	str = malloc(sizeof(char) * len);
+	if (str = 0)
+		return (NULL);
+}
