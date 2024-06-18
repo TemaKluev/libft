edeea2afca76d31602707eb1c081923704f7a22b
Artem Kliuiev <tartem_09@live.com>
2024-06-18 21:35:23 +0200

    ekmfiweom

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: artemkliuiev <artemkliuiev@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
-/*   Updated: 2024/06/17 23:18:40 by artemkliuie      ###   ########.fr       */
+/*   Updated: 2024/06/18 21:35:09 by artemkliuie      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,16 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	int		i;
 	char	*str;
 
-	str = malloc(sizeof(char) * len);
+	str = malloc(sizeof(char) * len + 1);
 	if (str = 0)
 		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
 }
