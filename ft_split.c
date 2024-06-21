967f31ca86a9deb9b8cccec5ae2a505141a89195
Artem Kliuiev <akliuiev@c3r3s3.42wolfsburg.de>
2024-06-21 19:46:56 +0200

    dawedfwe

--- ft_split.c
+++ ft_split.c
@@ -1,40 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akliuiev <akliuiev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 17:43:12 by akliuiev          #+#    #+#             */
-/*   Updated: 2024/06/21 18:02:06 by akliuiev         ###   ########.fr       */
+/*   Updated: 2024/06/21 19:08:23 by akliuiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
 	char	*str;
 
-	i = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc(sizeof(char) * i + 1);
+	str = 0;
+	if (s1 == 0 || set == 0)
+		return (NULL);
+	j = ft_strlen(s1) - 1;
+	i = 0;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (s1[j] && j >= i && ft_strchr(set, s1[j]))
+		j--;
+	str = malloc(sizeof(char) * (j - i));
 	if (str = 0)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[i] != '\0')
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	str[i] = '\0';
+	ft_strlcpy(str, s1[i], j - i + 1);
 	return (str);
 }
